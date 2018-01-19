425cb3ebb6c76ca7a79d258a9fc8c03ac8a65d27
DESKTOP-EBE9M9V\mathi <mathijs.kemp@eproseed.com>
2018-01-19 14:19:17 +0100

    De app

---
+++ APPER1/KaartView.cs
@@ -0,0 +1,235 @@
+﻿using System;             // Math
+using Android.Views;      // View & TouchEventArgs
+using Android.Hardware;   // SensorManager & ISensorEventListener
+using Android.Locations;  // Locationc & ILocationListener
+using Android.Graphics;   // Paint & Canvas & PointF
+using Android.Content;    // Context
+using Android.OS;         // Bundle
+using System.Collections.Generic;  // List
+
+
+namespace APPER1
+{
+
+    public class KaartView : View , ISensorEventListener , ILocationListener
+    {
+
+        public bool trainingGestart;  //public boolean die bijhoudt of de training gestart is of niet
+        Bitmap utrecht, loper;        // declaratie van de bitmaps van de kaart en het icoontje.
+        float Schaal;
+        public double noord, oost;
+        public bool volg = false;  //boolean om aan te duiden of de locatie gevolgd en getekend moet worden
+        public List<Opslaan> looppad = new List<Opslaan>();   //lijst van alle punten waar de gebruiker geweest is
+        float Hoek;                                    
+        PointF uithof = new PointF(142000, 459600);  // Uithof positie, uitgangspunt van de app
+        PointF centrum = new PointF(139000, 455500);  // Centrum van de kaart
+
+        public KaartView(Context c) : base(c)
+        {
+            this.SetBackgroundColor(Color.Black);
+            BitmapFactory.Options opt = new BitmapFactory.Options();
+            opt.InScaled = false;                                       // Zorgt ervoor dat de kaart oningezoomd wordt getoond.
+            utrecht = BitmapFactory.DecodeResource(this.Resources, Resource.Drawable.kaart, opt);   // Tekent de kaart
+            loper = BitmapFactory.DecodeResource(c.Resources, Resource.Drawable.point, opt);    // tekent de loper
+
+            SensorManager sm = (SensorManager)c.GetSystemService(Context.SensorService);             
+            sm.RegisterListener(this, sm.GetDefaultSensor(SensorType.Orientation), SensorDelay.Ui);
+
+            LocationManager lm = (LocationManager)c.GetSystemService(Context.LocationService);
+            Criteria crit = new Criteria();
+            crit.Accuracy = Accuracy.Fine;
+            string lp = lm.GetBestProvider(crit, true);
+            if (lp != null)
+                lm.RequestLocationUpdates(lp, 0, 0, this);
+            this.Touch += RaakAaan;
+        }
+      
+        protected override void OnDraw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+
+            if (Schaal == 0)
+                Schaal = Math.Min(((float)this.Width) / this.utrecht.Width, ((float)this.Height) / this.utrecht.Height);
+
+            Paint verf = new Paint();
+            float middenX = (centrum.X - 136000) / 2.5f;
+            float middenY = (458000 - centrum.Y) / 2.5f;
+
+            Matrix mat = new Matrix();                    
+            mat.PostTranslate(-middenX, -middenY);             // Zorgt ervoor dat de kaart op de goede plek wordt getekend
+            mat.PostScale(this.Schaal, this.Schaal);
+            mat.PostTranslate(this.Width / 2, this.Height / 2);
+            canvas.DrawBitmap(utrecht, mat, verf);
+            
+            mat = new Matrix();
+            mat.PostTranslate((-loper.Width) / 2, (-loper.Height) / 2);        // Zorgt ervoor dat de positie op de goede plek wordt getekend
+            mat.PostScale(this.Schaal / 9, this.Schaal / 9);
+            mat.PostRotate(-this.Hoek);
+            mat.PostTranslate((this.Width / 2 + (uithof.X - centrum.X) / 2.5f * this.Schaal), this.Height / 2 + (centrum.Y - uithof.Y) / 2.5f * this.Schaal);
+            canvas.DrawBitmap(this.loper, mat, verf);
+            Paint kleur = new Paint();
+            kleur.Color = Color.Red;
+            float r = 10;
+            r = r * this.Schaal;                                // Zorgt ervoor dat de pointer dezelfde grootte blijft bij het in- en uitzoomen
+            foreach (Opslaan punt in looppad)
+            {
+                
+                float bitmapx = (punt.x - centrum.X) / 2.5f;  // Opslaan punten omzetten in schermrelatieve pixels
+                float bitmapy = (centrum.Y - punt.y) / 2.5f;
+
+                float schermx = bitmapx * this.Schaal;
+                float schermy = bitmapy * this.Schaal;
+
+                float x = this.Width / 2 + schermx;
+                float y = this.Height / 2 + schermy;
+                mat.PostScale(this.Schaal, this.Schaal);
+                canvas.DrawCircle(x, y, r, kleur);          // Tekent het gelopen pad
+            }
+
+        }
+        public void OnSensorChanged(SensorEvent e)            // Methode die ervoor zorgt dat de pointer in de goede richting wordt getekend
+        {
+            this.Hoek = e.Values[0];
+            this.Invalidate();
+        }
+     
+        public void OnLocationChanged(Location location)     // Methode die ervoor zorgt dat er elke keer een nieuw punt wordt geregistreerd als de locatie is gewijzigd
+        {
+            PointF geo = new PointF((float)location.Latitude, (float)location.Longitude);
+            uithof = Projectie.Geo2RD(geo);
+            if (volg)
+            {
+                looppad.Add(new Opslaan(uithof)); 
+            }
+            this.Invalidate();
+        }
+        public void OnAccuracyChanged(Sensor s, SensorStatus accuracy)      
+        {
+        }
+
+        public void OnProviderEnabled(string s)
+        {
+        }
+
+
+
+        public void OnProviderDisabled(string s)
+        {
+
+        }
+
+        public void OnStatusChanged(string provider, Availability status, Bundle extras)
+        {
+        }
+        static float Afstand(PointF p1, PointF p2)         // Berekeningen voor het pinchen
+        {
+            float a = p2.X - p1.X;
+            float b = p2.Y - p1.Y;
+            return (float)Math.Sqrt(a * a + b * b);
+        }
+   
+        PointF start1;   // variabelen die nodig zijn voor het pinchen en draggen.
+        PointF start2;
+        PointF huidig1;
+        bool pinchen = false;
+        PointF huidig2;
+        float oudeSchaal;
+        PointF oudeCentrum;
+
+        public void RaakAaan(object o, TouchEventArgs tea)
+        {
+            huidig1 = new PointF(tea.Event.GetX(0), tea.Event.GetY(0));
+
+            if (tea.Event.Action == MotionEventActions.Down)
+            {
+                start1 = huidig1;
+                oudeCentrum = centrum;
+                oudeSchaal = Schaal;
+            }
+            if (tea.Event.PointerCount == 1)   // If-statement voor het pinchen
+            {
+
+
+                if (!pinchen)
+                {
+                    float dx = huidig1.X - start1.X;  // In scherm pixels de afstand tot midden 
+                    float dy = huidig1.Y - start1.Y;
+                    float ax = (dx / oudeSchaal) * 2.5f;  // In meters afstand tot midden
+                    float ay = (dy / oudeSchaal) * 2.5f;
+                    centrum = new PointF(oudeCentrum.X - ax, oudeCentrum.Y + ay);
+                    this.Invalidate();
+                }
+            }
+
+            if (tea.Event.PointerCount == 2)    // If-statement voor het draggen
+            {
+                pinchen = true;
+                huidig2 = new PointF(tea.Event.GetX(1), tea.Event.GetY(1));
+                if (tea.Event.Action == MotionEventActions.Pointer2Down)
+                {
+                    start1 = huidig1;
+                    start2 = huidig2;
+
+                }
+                float oud = Afstand(start2, start1);
+                float nieuw = Afstand(huidig2, huidig1);
+                if (oud != 0 && nieuw != 0)
+                {
+                    float factor = nieuw / oud;
+                    Schaal = oudeSchaal * factor;
+                    if (Schaal > 10) Schaal = 10;   // Limiteer schaal zodat er niet oneindig in en uitgezoomd kan worden
+                    if (Schaal < 0.1) Schaal = 0.1f;
+                }
+
+            }
+            if (tea.Event.Action == MotionEventActions.Up)
+                pinchen = false;                  // Zorgt ervoor dat je kan draggen na het pinchen
+
+            this.Invalidate();
+
+        }
+        public void Centreren(object o, EventArgs ea)  // Regelt de functionaliteit van de centreerknop
+        {
+            centrum = uithof;
+            this.Invalidate();
+        }
+
+        //eventhandler voor de start/stop knop
+        public void StartStop(object o, EventArgs ea)
+        {
+            volg = !volg;
+            this.Invalidate();
+        }
+
+        /*een van de eventhandlers voor de opschonen knop,
+          deze schoont daadwerkelijk het looppad op*/
+        public void Opschonen(object o, EventArgs ea)
+        {
+            looppad.Clear();
+            //zorgt ervoor dat de locatie niet meer gevolgd wordt
+            volg = false;
+            //zorgt ervoor dat de correcte dialoog laten zien wanneer de opschonen knop is ingedrukt
+            trainingGestart = false;
+            this.Invalidate();
+        }
+        // Opslaan van alle punten gebeurt in een klasse
+        public class Opslaan
+        {
+            public float x;
+            public float y;
+
+            public Opslaan(PointF p)
+            {
+                x = p.X;
+                y = p.Y;
+            }
+
+            public Opslaan(float x, float y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+        }
+    }
+}
---
+++ APPER1/MainActivity.cs
@@ -0,0 +1,186 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics;
+using Android.Views;
+using Android.Content;
+using System;
+using Android.Hardware;
+using Android.Locations;
+using Android.Runtime;
+
+namespace APPER1
+{
+    [Activity(Label = "HardloopApp", MainLauncher = true , Icon = "@drawable/icon")]
+    public class MainActivity : Activity , ILocationListener
+    {
+        TextView coordinaten;   
+        KaartView utrecht;
+        Button startstop;
+        
+        protected override void OnResume()
+        {
+            base.OnResume();
+            // Gps Signaal zoeken voor de tekst
+            LocationManager lm = (LocationManager)base.GetSystemService(Context.LocationService);
+            Criteria crit = new Criteria();
+            crit.Accuracy = Accuracy.Fine;
+            string lp = lm.GetBestProvider(crit, true);
+            if (lp != null)
+                lm.RequestLocationUpdates(lp, 0, 0, this);
+        }
+        protected override void OnCreate(Bundle b)
+        {
+            base.OnCreate(b);
+
+            //verwijzing naar klasse KaartView
+            utrecht = new KaartView(this);
+            KaartView kaartview = new KaartView(this);
+
+
+            coordinaten = new TextView(this);
+            coordinaten.SetBackgroundColor(Color.Black);
+            coordinaten.Text = "Zoeken naar GPS...";
+            
+            Button centreer = new Button(this);
+            centreer.Text = "Centreer";
+            centreer.SetTextColor(Color.White);
+
+            //eventhandler voor de centreerknop in de kaartview klasse
+            centreer.Click += utrecht.Centreren;
+
+            //declaraties voor de start/stop knop
+            startstop = new Button(this);
+            startstop.Text = "Start";
+            startstop.SetTextColor(Color.White);
+
+            //declaratie van de eventhandler in de MainActivity
+            startstop.Click += this.StartStop;
+
+            //declaratie van de eventhandler in de kaartview klasse
+            startstop.Click += utrecht.StartStop;
+
+            //declaraties voor de opschonen knop
+            Button opschonen = new Button(this);
+            opschonen.Text = "Opschonen";
+            opschonen.SetTextColor(Color.White);
+
+            //declaratie van de eventhandler in de MainActivity
+            opschonen.Click += this.Opschonen;
+            
+            LinearLayout horizontaal = new LinearLayout(this);
+            horizontaal.Orientation = Orientation.Horizontal;
+            horizontaal.AddView(centreer);
+            horizontaal.AddView(startstop);
+            horizontaal.AddView(opschonen);
+
+            LinearLayout verticaal = new LinearLayout(this);
+            verticaal.Orientation = Orientation.Vertical;
+            verticaal.AddView(horizontaal);
+            verticaal.AddView(coordinaten);
+            verticaal.AddView(utrecht);
+
+            this.SetContentView(verticaal);
+        }
+        public void StartStop(object o, EventArgs ea)  // EventHandler voor de Start/Stop knop
+        {
+           
+            if (!utrecht.volg)
+            {
+                startstop.Text = "Stop";
+                utrecht.trainingGestart = true;
+            }
+            else if (utrecht.volg)
+            {
+                startstop.Text = "Start";
+                utrecht.trainingGestart = false;
+            }
+        }
+
+        //eventhandler voor de opschonen knop
+        public void Opschonen(object o, EventArgs ea) {
+
+         
+            if (utrecht.looppad.Count != 0){
+                startstop.Text = "Start";
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.SetTitle("Weet je het zeker?");
+                alert.SetMessage("Weet je zeker dat je jouw training wilt opschonen?");
+                alert.SetPositiveButton("Ja", utrecht.Opschonen);
+                alert.SetNegativeButton("Nee", this.Cancel);
+                alert.Create().Show();
+            }
+            else 
+            {
+                AlertDialog.Builder popup = new AlertDialog.Builder(this);
+                popup.SetTitle("Training nog niet gestart");
+                popup.SetMessage("Er is een fout opgetreden. De training kan niet worden opgeschoond, als de training nog niet is gestart!");
+                popup.SetPositiveButton("OK", this.Cancel);
+                popup.Create().Show();
+            }
+        }
+
+       
+        public void Cancel(object o, EventArgs ea)  // Eventhandler voor de nee/OK knop in de dialoog.
+        {
+          
+             if (utrecht.looppad.Count != 0)
+            {
+                if (utrecht.volg)
+                {
+                    startstop.Text = "Stop";
+                    utrecht.volg = true;
+                }
+                else if (!utrecht.volg)
+                {
+                    startstop.Text = "start";
+                    utrecht.volg = false;
+                }
+            }
+             else
+            {
+                if (utrecht.looppad.Count == 0)
+                {
+                    startstop.Text = "Start";
+                    utrecht.volg = false;
+                }
+                else
+                {
+                    startstop.Text = "Stop";
+                    utrecht.volg = false;
+                }
+            }
+       
+       
+        }
+        public void OnLocationChanged(Location location)       // Zorgt ervoor dat de coordinaten goed worden opgeslagen en weergegeven voor de gebruiker
+        {
+            utrecht.noord = location.Latitude;
+            utrecht.oost = location.Longitude;
+            String info = $"{utrecht.noord} graden noorderbreedte, {utrecht.oost} graden oosterlengte";
+            coordinaten.Text = info;
+
+        }
+        // Benodigde methoden die verplicht aangeroepen moeten worden
+        public void OnProviderDisabled(string provider)
+        {
+          
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+           
+        }
+
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
+        {
+           
+        }
+    }
+}
+
+
+
+
+
+
